64a6910e0670d8b9fde66b0fba6334873421a4b4
jack1234 <jackdoson2011@gmail.com>
2019-01-02 23:36:44 +0800

    test

--- app/src/test/java/com/example/jacky/clickhookdemo/ExampleTest.java
+++ app/src/test/java/com/example/jacky/clickhookdemo/ExampleTest.java
@@ -2,7 +2,12 @@ package com.example.jacky.clickhookdemo;
 
 import org.junit.Test;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class ExampleTest {
+    private final static Pattern pattern = Pattern.compile("\\[[^\\]]+\\]", Pattern.CASE_INSENSITIVE);
+
     @Test
     public void letter_index_test() {
         for (int i = 0; i < 10000; i++) {
@@ -18,5 +23,21 @@ public class ExampleTest {
 
         int i = '1';
         Util.println(i);
+    }
+
+    @Test
+    public void regex_test() {
+        String source = "[大笑][大哭]hello[123]";
+        final Matcher matcher = pattern.matcher(source);
+
+        int start;
+        for (start = 0; start < source.length(); start++) {
+            if (matcher.find()) {
+                String key = matcher.group();
+                start = matcher.start() + key.length();
+                Util.println("start=" + start + ",\t key=" + key);
+                Util.println("result=" + matcher.replaceFirst("AB"));
+            }
+        }
     }
 }
